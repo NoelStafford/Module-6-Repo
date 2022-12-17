731dc4a611116acc56eb639bd7a299904688166b
NoelStafford <noeljordan34@gmail.com>
2022-12-16 19:31:16 -0600

    second attempt

--- assests/Javascipt/script.js
+++ assests/Javascipt/script.js
@@ -1,5 +1,69 @@
+// five day forecase api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// current weather https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const apiKey = '849f1b4e3f695924e81cda9efcd975ec';
 
-// api key for current weather https://api.openweathermap.org/data/2.5/weather?lat=  &appid=
+var lat;
+var lon;
 
 
-// api key for geo tag http://api.openweathermap.org/geo/1.0/direct?q=
+
+const search = document.getElementById("city-search")
+const searchbtn = document.getElementById("search-button")
+searchbtn.addEventListener("click", searchWeather)
+
+function searchWeather() {
+    var city = search.value
+var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=" + apiKey;
+fetch (currentWeather)
+.then(function (response){
+    return response.json()
+})
+.then (function (response){
+    console.log(response)
+    document.getElementById("temp0").textContent = "Temp: " + response.main.temp + "*F"
+    document.getElementById("wind0").textContent = "Wind: " + response.wind
+
+
+
+
+
+
+
+
+get5day(city)
+})
+}
+
+function get5day(city) {   
+var currentWeather = " https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&units=imperial&appid=" + apiKey;
+fetch (currentWeather)
+.then(function (response){
+    return response.json()
+})
+.then (function (response){
+    console.log(response)
+    document.getElementById("temp1").textContent = "Temp: " + response.list[0].main.temp + "*F"
+    document.getElementById("date1").textContent = "Date: " + response.list[0].dt_text
+    document.getElementById("wind1").textContent = "Wind: " + response.list[0].wind
+
+
+
+    document.getElementById("temp2").textContent = "Temp: " + response.list[8].main.temp
+     + "*F"
+    document.getElementById("temp2").textContent = "Temp: " + response.list[16].main.temp
+     + "*F"
+    document.getElementById("temp2").textContent = "Temp: " + response.list[24].main.temp 
+    + "*F"
+    document.getElementById("temp2").textContent = "Temp: " + response.list[32].main.temp + "*F"
+
+
+
+
+
+
+
+
+
+
+})
+}
